9fd2c345a0f76c91a741bb8e3c5a66b55bd9a573
ftptpf <Nb49Rtdd>
2022-12-25 13:50:24 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -7,10 +7,19 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
+
+        Store store = context.getBean(Store.class);
+        store.add("Dmitriy Dmitriev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
+
         StartUI ui = context.getBean(StartUI.class);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
         ui.askStr("Some text from ConsoleInput. Please press Enter.");
+        StartUI uiOther = context.getBean(StartUI.class);
+        uiOther.add("Sergey Sergeev");
+        uiOther.print();
     }
 }
